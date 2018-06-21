40f27dbbd22a7b6e6f6d712bdc78fde76323a6bd
Andrew James <ajames20@gmail.com>
2018-06-21 13:45:27 -0600

    Add stock prices and fix centering

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -23,6 +23,22 @@ class Main extends Component {
           latestPrice: 1161.44,
           change: -8.4,
         },
+        {
+          name: 'Apple Inc.',
+          shortName: 'AAPL',
+          high: 188.35,
+          low: 185.93,
+          latestPrice: 185.24,
+          change: 1.26,
+        },
+        {
+          name: 'Alphabet Inc.',
+          shortName: 'GOOG',
+          high: 1177.295,
+          low: 1153.83,
+          latestPrice: 1161.44,
+          change: -8.4,
+        },
       ],
     };
   }
--- src/components/StockCard/index.js
+++ src/components/StockCard/index.js
@@ -14,14 +14,14 @@ const StockCard = props => {
         {change < 0 && (
           <p className="stock-card__change negative-change">
             <span className="stock-card__pointer"> &#x25bc;</span>
-            {change}
+            {change} ({(high / latestPrice).toFixed(2)}%)
           </p>
         )}
 
         {change > 0 && (
           <p className="stock-card__change positive-change">
             <span className="stock-card__pointer"> &#x25b2;</span>
-            {change}
+            {change} ({(high / latestPrice).toFixed(2)}%)
           </p>
         )}
 
@@ -29,8 +29,11 @@ const StockCard = props => {
       </div>
 
       <div className="stock-card-average">
-        <p>{high}</p>
-        <p>{low}</p>
+        <span className="stock-metric" style={{ top: high.toFixed(2) / 100 - change }}>
+          &#x25ba;
+        </span>
+        <p>{high.toFixed(2)}</p>
+        <p>{low.toFixed(2)}</p>
       </div>
     </section>
   );
